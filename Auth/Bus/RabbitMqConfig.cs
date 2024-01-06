ca49611a0859e5c8b6d84bac3606c80c986635ff
MohammadYSF <aa.yosefiyan7@gmail.com>
2024-01-06 03:40:22 +0330

    reading rabbit port from config

--- Auth/Bus/RabbitMqConfig.cs
+++ Auth/Bus/RabbitMqConfig.cs
@@ -5,4 +5,5 @@ public class RabbitMqConfig
     public string HostName { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
+    public int Port { get; set; }
 }
